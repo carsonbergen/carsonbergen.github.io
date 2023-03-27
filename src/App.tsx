c3cd6cdee9909aa82de3b44bacb1f673c61f6bde
Carson Bergen <111704590+carsonbergen@users.noreply.github.com>
2023-03-27 00:35:29 -0600

    Added footer, edited navbar, and create home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Blog from './pages/Blog';
 import GameDevCorner from './pages/GameDevCorner';
 import Home from './pages/Home';
@@ -11,14 +12,17 @@ import Projects from './pages/Projects';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/blog' element={<Blog />} />
-        <Route path='/projects' element={<Projects />} />
-        <Route path='/game_dev_corner' element={<GameDevCorner />} />
-      </Routes>
+    <div >
+      <div className='w-screen h-screen justify-center'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/game_dev_corner' element={<GameDevCorner />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Prism } from '@mantine/prism';
 
-const info_text = `
+const welcome_info_text = `
 Welcome to my personal website. 
 
 My name is Carson Bergen, and I’m currently studying computer science at the University of Calgary.
@@ -16,19 +17,45 @@ I have quite a few interests, but at the moment I’m focusing on:
 Feel free to check out my blog, my projects, or my “Game dev corner” (where I post anything game development related).
 `
 
+const hobbies_info_text = `
+My main hobbies included:
+  - Learning languages (currently working on becoming fluent in Russian, and intermediate in Spanish)
+  - Game development
+  - 3D modelling
+`
+const game_dev_info_text = (
+  <article className="prose-stone">
+    I'm currently working on developing a FPS game in Godot.
+  </article>
+)
+
 function Home() {
   return(
     <>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row">
-          <div>
-            <h1 className="text-5xl font-bold">Welcome!</h1>
-            <p className="py-6 whitespace-pre">{ info_text }</p>
-            <button className="btn btn-primary">
-              <Link to="/projects">Check out my projects!</Link>
-            </button>
+      <div className="flex flex-col space-y-4 items-center">
+        <div className="card w-[72rem] max-w-6xl bg-gray-700 text-primary-content">
+            <div className="card-body">
+              <h2 className="card-title">Welcome!</h2>
+              <p className="whitespace-pre-wrap">{ welcome_info_text }</p>
+              <Link to='/projects'>Check out my projects!</Link>
+            </div>
           </div>
-        </div>
+          <div className="flex flex-row">
+            <div className="card w-[27rem] bg-gray-700 text-primary-content">
+              <div className="card-body">
+                <h2 className="card-title">Hobbies:</h2>
+                <p className="whitespace-pre-wrap">{ hobbies_info_text }</p>
+                <Link to='https://carsonbergen.itch.io/'>Check out my itch.io page!</Link>
+              </div>
+            </div>
+            <div className="card w-[44rem] bg-gray-700 text-primary-content ml-4">
+              <div className="card-body">
+                <h2 className="card-title">Game Development</h2>
+                <p className="whitespace-pre-wrap">{ game_dev_info_text }</p>
+                <Link to='/game_dev_corner'>Check out my game development corner!</Link>
+              </div>
+            </div>
+          </div>
       </div>
     </>
   );
