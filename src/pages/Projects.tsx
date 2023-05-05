11567bb61beeb6d10ab7b5fe57b79cee6520b7c5
Carson Bergen <111704590+carsonbergen@users.noreply.github.com>
2023-05-04 23:06:03 -0600

    Added whimsical wizardry images

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,3 +1,5 @@
+import CarouselImage from "../components/CarouselImage";
+import ImageCarousel from "../components/ImageCarousel";
 import Warning from "../components/Warning";
 
 function Projects() {
@@ -12,6 +14,26 @@ function Projects() {
                         <p className="page-paragraph">
                             This is a game I started working on, involving wizards, and loads of whimsical endeavours. 
                         </p>
+                        <ImageCarousel>
+                            <CarouselImage 
+                                id="slide1" 
+                                nextId="#slide2"
+                                prevId="#slide3" 
+                                src="src/assets/whimsical_wizardry/idle_animation_test.gif" 
+                            />
+                            <CarouselImage 
+                                id="slide2" 
+                                nextId="#slide3"
+                                prevId="#slide1" 
+                                src="src/assets/whimsical_wizardry/walk_animation_test.gif" 
+                            />
+                            <CarouselImage 
+                                id="slide3" 
+                                nextId="#slide1"
+                                prevId="#slide2" 
+                                src="src/assets/whimsical_wizardry/idle_animation_test_2.gif" 
+                            />
+                        </ImageCarousel>
                     </div>
                 </div>
             </div>
