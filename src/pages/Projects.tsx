3ffeb3eca8dbec21a551358518f7d62d6f465838
Carson Bergen <111704590+carsonbergen@users.noreply.github.com>
2023-05-06 10:08:26 -0600

    Added game development corner styling and content

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -6,30 +6,6 @@ function Projects() {
     return (
         <>
             <div className='page-body'>
-                <div className="card">
-                    <div className="card-body">
-                        <h1 className="card-title">
-                            Project Whimsical Wizardry
-                        </h1>
-                        <p className="page-paragraph">
-                            This is a game I started working on, involving wizards, and loads of whimsical endeavours. 
-                        </p>
-                        <div className="image-container">
-                            <img 
-                                className="image"
-                                src="/assets/whimsical_wizardry/idle_animation_test_2.gif"
-                                alt="Wizard idle animation test 2 gif"
-                                width="400"
-                            />
-                            <img 
-                                className="image"
-                                src="/assets/whimsical_wizardry/idle_animation_test.gif"
-                                alt="Wizard idle animation test 2 gif"
-                                width="400"
-                            />
-                        </div>
-                    </div>
-                </div>
             </div>
         </>
     );
